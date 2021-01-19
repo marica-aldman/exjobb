ead74b2dd135d73b5df5ce809b60c57ab7432967
marica-aldman <marica.aldman@gmail.com>
2021-01-19 12:23:38 +0100

    testing .production

--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -3,6 +3,6 @@ from django.conf import settings
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
-os.environ["DJANGO_SETTINGS_MODULE"] = "djecommerce.settings.production"
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings.production')
+#os.environ["DJANGO_SETTINGS_MODULE"] = "djecommerce.settings.production"
 application = get_wsgi_application()
