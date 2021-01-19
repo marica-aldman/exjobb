1e70924f9eb288da713a2221d75d7f0b82860be1
marica-aldman <marica.aldman@gmail.com>
2021-01-19 12:13:57 +0100

    testing wsgi

--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -3,5 +3,6 @@ from django.conf import settings
 from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "djecommerce.settings"
 application = get_wsgi_application()
