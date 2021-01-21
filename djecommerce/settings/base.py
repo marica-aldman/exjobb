6e62a00237a9b3bea24a674116989057dc3d3044
marica-aldman <marica.aldman@gmail.com>
2021-01-21 14:21:19 +0100

    warning for static files

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -69,7 +69,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_in_env')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 # Auth
